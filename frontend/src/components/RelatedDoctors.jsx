b35a92f83075fef3a91adac59f12c7abcca6fc32
Akash kumar <akashgkr12@gmail.com>
2025-10-04 04:55:07 +0530

    component complete

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -1,13 +1,24 @@
 
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import { useNavigate } from 'react-router-dom';
 
-import { useNavigate } from 'react-router-dom'
-import { AppContext } from '../context/AppContext';
+const RelatedDoctors = ({docId,speciality}) => {
 
-const TopDoctors = () => {
-
-  const {doctors}=useContext(AppContext);
   const navigate=useNavigate();
+  const {doctors}=useContext(AppContext);
+  const [relDoc,setRelDoc]=useState([]);
+
+ 
+
+  useEffect(()=>{
+  if(doctors.length>0 && speciality){
+    // without parentheses (allowed if only one parameter)
+    // with parentheses around parameter
+  const doctorsData=doctors.filter((doc)=>doc.speciality === speciality && doc._id !== docId);
+  setRelDoc(doctorsData);
+  }
+  },[doctors,speciality,docId]);
 
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
@@ -15,7 +26,7 @@ const TopDoctors = () => {
       <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
       <p className="sm:w-1/3 text-center text-sm">Simply browse through our extensive list of trusted doctors.</p>
       <div className="w-full grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 pt-5 gap-y-6 px-3 sm:px-0">
-        {doctors.slice(0,10).map((item,index)=>(
+        {relDoc.slice(0,5).map((item,index)=>(
         <div onClick={()=>{navigate(`/appointment/${item._id}`);scrollTo(0,0)}} className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer
         hover:translate-y-[-10px] transition-all duration-500 " key={index}>
           <img className='bg-blue-50' src={item.image} alt="" />
@@ -34,4 +45,4 @@ const TopDoctors = () => {
   )
 }
 
-export default TopDoctors
+export default RelatedDoctors
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,1 +1,26 @@
+import { createContext } from "react";
+import { doctors } from "../assets/assets";
 
+export const AppContext=createContext();
+
+const AppContextProvider=({children})=>{ // direct destructuring using {}
+
+  const currencySymbol="$";
+
+  const value={
+    doctors,
+    currencySymbol
+  };
+
+  return (
+    <AppContext.Provider value={value}>
+      
+      {children} 
+      
+    </AppContext.Provider>
+  )
+};
+
+
+
+export default AppContextProvider;
